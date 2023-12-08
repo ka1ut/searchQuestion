5c8d3b1cd0e110f56204715e24466ea9e3ab3699
ka1ut <tka1utjp@gmail.com>
2023-12-08 23:21:14 +0900

    Initial Commit

---
+++ searchQuestion/searchQuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchQuestionApp.swift
+//  searchQuestion
+//
+//  Created by 田中魁 on 2023/12/08.
+//
+
+import SwiftUI
+
+@main
+struct searchQuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
